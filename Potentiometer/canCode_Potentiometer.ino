8b21c349b60b9cdd960664a53f61ebc577a4c2bd
mitchell <mitchellpeh@outlook.com>
2017-03-16 23:31:53 -0400

    documentation update

--- Potentiometer/canCode_Potentiometer.ino
+++ Potentiometer/canCode_Potentiometer.ino
@@ -1,11 +1,13 @@
 int potPin =0;    // select the input pin for the potentiometer
-int ledPin = 13;   // select the pin for the LED
+int ledPin = 13;   // select the pin for the LED (13 is the on-board LED for the Uno)
 int val = 0;       // variable to store the value coming from the sensor
 
+//remember, setup code runs once
 void setup() {
   pinMode(ledPin, OUTPUT);  // declare the ledPin as an OUTPUT
 }
 
+//loop code continously runs
 void loop() {
   val = analogRead(potPin);    // read the value from the sensor
   digitalWrite(ledPin, HIGH);  // turn the ledPin on
