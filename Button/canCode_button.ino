b2d292d36bae41114d5566f09830875b2c7093df
mitchell <mitchellpeh@outlook.com>
2017-03-17 11:43:41 -0400

    button code

--- Button/canCode_button.ino
+++ Button/canCode_button.ino
@@ -1,1 +1,28 @@
+//we need to set the pin numbers for our inputs and outputs
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin (pin 13 is the on-board for the Uno)
 
+// creating a variable to hold the state of the button (pressed/unpressed, 1/0 , high/low)
+int buttonState = 0;         // variable for reading the pushbutton status, button is low by default, so we set it to 0
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
